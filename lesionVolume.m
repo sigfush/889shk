9f8ea4bd18691f60327134d038fab8d6e469d143
sigfush <53525438+sigfush@users.noreply.github.com>
2019-07-31 10:24:37 -0400

    Add files via upload

---
+++ lesionVolume.m
@@ -0,0 +1,17 @@
+function lesionVolume(fnms)
+
+if ~exist('fnms','var')
+    fnms = spm_select(inf,'.mat','Select image[s] for NaN removal');
+end
+
+for i=1:size(fnms,1)
+    fnm = deblank(fnms(i,:));
+    lesionVolumeSub(fnm);
+end
+
+function lesionVolumeSub(fnm)
+m = load(fnm);
+if ~isfield(m,'lesion'), return; end
+vol = sum(m.lesion.dat(:));
+totalVol = numel(m.lesion.dat(:));
+fprintf('%s\t%d\n', vol, totalVol);
